e153ab58ab1ad685b0e120c574961daacaddda1f
Tony Glover <tony.glover@nottingham.ac.uk>
2016-07-07 02:27:08 +0100

    New facial model

--- js/points.js
+++ js/points.js
@@ -42,27 +42,31 @@ Motivate.prototype.createScene = function() {
     }
 
     this.facialFeatures = [
-        { feature: "topLip", point: 51, vertices: [1659, 14, 17, 1658, 13, 16, 1657, 12, 15, 1656, 1, 11, 1718, 63,  107, 1719, 64, 107,
-            1720, 65, 108] },
-        { feature: "topLipRight1", point: 52, vertices: [281, 266, 278, 279, 277, 280, 60, 58, 61, 59, 45, 44, 42]},
-        { feature: "topLipRight2", point: 53, vertices: [248, 1121, 264, 246, 608, 258, 57, 56, 55, 52, 51, 43, 50]},
-        { feature: "topLipRight3", point: 54, vertices: [245, 253, 257, 254, 54, 53, 49, 47, 48, 46, 22]},
-        { feature: "topLipLeft1", point: 50, vertices: [1858, 1873, 1871, 1870, 1872, 1869, 1700, 1702, 1701, 1703, 1686, 1687, 1684]},
-        { feature: "topLipLeft2", point: 49, vertices: [1840, 2704, 1856, 1838, 2200, 1850, 1699, 1698, 1697, 1694, 1693, 1685, 1692]},
-        { feature: "topLipLeft3", point: 48, vertices: [1837, 1845, 1849, 1846, 1696, 1695, 1691, 1689, 1690, 1688, 1664]},
-        { feature: "bottomLip", point: 57, vertices: [1652, 3, 5, 1651, 2, 4, 1653, 6, 7, 1655, 1, 9, 1654, 0, 8]}
-        /*
-        { feature: "bottomLipRight1", point: 56, vertices: []},
-        { feature: "bottomLipRight2", point: 55, vertices: []},
-        { feature: "bottomLipLeft1", point: 58, vertices: []},
-        { feature: "bottomLipLeft2", point: 59, vertices: []},
-        { feature: "MouthTopRight", point: 62, vertices: []},
-        { feature: "MouthTop", point: 61, vertices: []},
-        { feature: "MouthTopLeft", point: 60, vertices: []},
-        { feature: "MouthBottom", point: 64, vertices: []},
-        { feature: "MouthBottomRight", point: 63, vertices: []},
-        { feature: "MouthBottomLeft", point: 65, vertices: []}
-        */
+
+        { feature: "topLip", point: 51, vertices: [3413, 6950, 3412, 6470, 14, 6468, 3231, 6469, 3230, 6465, 13, 6463, 3229, 6464, 3228,
+            6461, 12, 6458, 3227, 6459, 3226, 6456, 10, 6455] },
+        { feature: "topLipRight1", point: 52, vertices: [6755, 3582, 7468, 3586, 106, 7470, 277, 7478, 6949, 3584, 7474, 4246, 17, 6634,
+            60, 6628, 6467, 3302, 6629, 3300, 16, 6635, 61, 6630, 6462, 3310, 6640, 3306, 15, 6642, 45, 6571, 6458, 3308, 6573, 3276,
+            11, 6566]}
+        //{ feature: "topLipRight2", point: 53, vertices: [248, 1121, 264, 246, 608, 258, 57, 56, 55, 52, 51, 43, 50]},
+        //{ feature: "topLipRight3", point: 54, vertices: [245, 253, 257, 254, 54, 53, 49, 47, 48, 46, 22]},
+        //{ feature: "topLipLeft1", point: 50, vertices: [1858, 1873, 1871, 1870, 1872, 1869, 1700, 1702, 1701, 1703, 1686, 1687, 1684]},
+        //{ feature: "topLipLeft2", point: 49, vertices: [1840, 2704, 1856, 1838, 2200, 1850, 1699, 1698, 1697, 1694, 1693, 1685, 1692]},
+        //{ feature: "topLipLeft3", point: 48, vertices: [1837, 1845, 1849, 1846, 1696, 1695, 1691, 1689, 1690, 1688, 1664]}
+
+
+        //{ feature: "bottomLip", point: 57, vertices: [1652, 3, 5, 1651, 2, 4, 1653, 6, 7, 1655, 1, 9, 1654, 0, 8]}
+        //{ feature: "bottomLipRight1", point: 56, vertices: []},
+        //{ feature: "bottomLipRight2", point: 55, vertices: []},
+        //{ feature: "bottomLipLeft1", point: 58, vertices: []},
+        // { feature: "bottomLipLeft2", point: 59, vertices: []},
+        //{ feature: "MouthTopRight", point: 62, vertices: []},
+        //{ feature: "MouthTop", point: 61, vertices: [6461, 12, 6458, 3227, 6459, 3226, 6456, 10, 6455]},
+        //{ feature: "MouthTopLeft", point: 60, vertices: [1693, 6599, 1694, 6592, 3285, 6598, 1692, 6595, 1685]}
+        //{ feature: "MouthBottom", point: 64, vertices: []},
+        //{ feature: "MouthBottomRight", point: 63, vertices: []},
+        //{ feature: "MouthBottomLeft", point: 65, vertices: []}
+
     ];
 
     //Create points
@@ -70,11 +74,11 @@ Motivate.prototype.createScene = function() {
 
     this.loader = new THREE.JSONLoader();
     var _this = this, mesh, material;
-    this.loader.load("models/maleHead.js", function(geom, mat) {
-        material = new THREE.MeshLambertMaterial({color:0x696969, wireframe:false});
-        mesh = new THREE.Mesh(geom, material);
+    this.loader.load("models/maleHeadTextured.js", function(geom, mat) {
+        //material = new THREE.MeshLambertMaterial({color:0x696969, wireframe:false});
+        mesh = new THREE.Mesh(geom, new THREE.MultiMaterial( mat ));
         _this.origGeom = geom.clone();
-        mesh.rotation.x = Math.PI/2;
+        //mesh.rotation.x = Math.PI/2;
         _this.scene.add(mesh);
         _this.headMesh = mesh;
     });
@@ -112,12 +116,12 @@ Motivate.prototype.resetFrames = function() {
 Motivate.prototype.createGUI = function() {
     var _this = this;
     this.guiControls = new function() {
-        this.Scale = 0.05;
+        this.Scale = 0.01;
     };
 
     //Create GUI
     var gui = new dat.GUI();
-    var scale = gui.add(this.guiControls, 'Scale', 0, 1).step(0.01);
+    var scale = gui.add(this.guiControls, 'Scale', 0, 0.1).step(0.001);
     scale.listen();
 };
 
